17dad08cde707184526516d9d69cfc19e3656e93
Noah Hendrickson <noah@noahhendrickson.com>
2026-03-04 18:34:23 -0500

    Enable async and scheduling on RuntimeApplication

--- runtime/src/main/java/com/noahhendrickson/runtime/RuntimeApplication.java
+++ runtime/src/main/java/com/noahhendrickson/runtime/RuntimeApplication.java
@@ -2,7 +2,11 @@ package com.noahhendrickson.runtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = "com.noahhendrickson")
 public class RuntimeApplication {
 
