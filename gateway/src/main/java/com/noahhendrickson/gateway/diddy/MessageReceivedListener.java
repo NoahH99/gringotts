dae9b52a3cb50a0e113af49e8655454d691c89ee
NoahH99 <noah@noahhendrickson.com>
2026-03-05 15:54:36 -0500

    Extracted some log strings to constants, also fixed white space issues.

--- gateway/src/main/java/com/noahhendrickson/gateway/diddy/MessageReceivedListener.java
+++ gateway/src/main/java/com/noahhendrickson/gateway/diddy/MessageReceivedListener.java
@@ -1,5 +1,6 @@
 package com.noahhendrickson.gateway.diddy;
 
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -11,20 +12,23 @@ import org.springframework.stereotype.Component;
 public class MessageReceivedListener extends ListenerAdapter {
 
     private static final Logger log = LoggerFactory.getLogger(MessageReceivedListener.class);
+    private static final String DIDDY_GIF = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG13b3o2bHdveGZme" +
+            "GZzdWIyeWNmeWlmMTAwYzg3amhpbGJjcGhkOCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/KtLmwz91Cjf5KvfVgd/giphy.gif";
+    private static final String LUBE_BOTTLE_EMOJI = " \uD83E\uDDF4";
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        if (event.getAuthor().isBot() || event.isWebhookMessage()) return;
+        User author = event.getAuthor();
+
+        if (author.isBot() || event.isWebhookMessage()) return;
         if (!event.isFromGuild()) return;
         if (!event.getMessage().getContentStripped().toLowerCase().contains("lube")) return;
 
-
         MessageChannelUnion channel = event.getChannel();
-        log.info(event.getAuthor().getName()+" " + event.getMessage().getContentStripped());
-        channel.sendMessage("DID SOMEONE SAY LUBE!!! WHERES THE INV \uD83E\uDDF4").queue();
-        channel.sendMessage("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcG13b3o2bHdveGZmeGZzdWIyeWNmeWlmMTAwYzg3amhpbGJjcGhkOCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/KtLmwz91Cjf5KvfVgd/giphy.gif").queue();
 
+        channel.sendMessage("DID SOMEONE SAY LUBE!!! WHERES THE INV " + LUBE_BOTTLE_EMOJI).queue();
+        channel.sendMessage(DIDDY_GIF).queue();
 
-
+        log.info("User {} ({}) said: {}", author.getName(), author.getId(), event.getMessage().getContentRaw());
     }
 }
